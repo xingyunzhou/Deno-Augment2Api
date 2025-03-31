9b31aea7dd1b80f63eb68c2eb371e8267fd82a82
lpp <350331045@qq.com>
2025-03-31 14:35:11 +0800

    删除http

--- main.ts
+++ main.ts
@@ -20,7 +20,6 @@ import {
 } from "./types.ts";
 
 const kv = await Deno.openKv();
-// const kv = await Deno.openKv("https://api.deno.com/databases/b327ac9a-fc76-43ff-88f2-30627830d980/connect");
 
 const app = new Application();
 const router = new Router();
@@ -234,16 +233,16 @@ const chatCompletionsHandler = async (ctx: any) => {
 }
 
 router.post("/v1", async (ctx) => {
-  return await chatCompletionsHandler(ctx)
+  await chatCompletionsHandler(ctx)
 });
 
 router.post("/v1/chat", async (ctx) => {
-  return await chatCompletionsHandler(ctx)
+  await chatCompletionsHandler(ctx)
 });
 
 //v1/chat/completions
 router.post("/v1/chat/completions", async (ctx) => {
-  return await chatCompletionsHandler(ctx)
+  await chatCompletionsHandler(ctx)
 });
 
 // 处理流式请求
